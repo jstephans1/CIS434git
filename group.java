c00d07f40d960852080aebd5d01e8c6bf16f4eca
Om-Patel01 <163884490+Om-Patel01@users.noreply.github.com>
2024-03-29 11:48:54 -0400

    Update group.java
    Added a method to return my name and ID

--- group.java
+++ group.java
@@ -2,6 +2,10 @@ public class group {
   public static void main(String[] Args) {
   }
 
+  static String OmPatelInfo() {
+  return ("Om Patel CSU ID: 2772347 ");
+  }
+  
   static String getGroupMembers() { 
   }
   
