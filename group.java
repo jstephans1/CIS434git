634f6eff6fb9c3b7e683c7eb1afadd1f32a6c332
EwiSkywalker <etgulley65@gmail.com>
2024-04-03 13:04:58 -0400

    result not initialized

--- group.java
+++ group.java
@@ -44,6 +44,7 @@ public class group {
   
   public static int multiply(int num1, int num2){
           boolean negative;
+          int result = 0;
           if(num1==0 || num2==0){
               return 0;
           }
